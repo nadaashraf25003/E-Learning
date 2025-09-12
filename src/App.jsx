01ed0a75b1766b61e8e6b9c653ce9132dfef947d
Mai Mohamed <mailalala4542@gmail.com>
2025-09-12 21:58:05 +0300

    Mai

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import Home from './Home.jsx'
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    <>
-      p
-      
-    </>
+    
+      <Home/>
+   
   )
 }
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,35 @@
+const NavBar = () => {
+    return ( 
+        <>
+            <header className="totc-header">
+      <nav className="totc-nav">
+        <div className="flex items-center space-x-2">
+          <div >
+            <span className="uplearn-text">UpLearn</span>
+          </div>
+        </div>
+
+        <div className="totc-nav-links">
+          <a href="#" className="totc-nav-link">
+            Home
+          </a>
+          <a href="#" className="totc-nav-link">
+            Courses
+          </a>
+          <a href="#" className="totc-nav-link">
+            Careers
+          </a>
+          <a href="#" className="totc-nav-link">
+            Blog
+          </a>
+          <a href="#" className="totc-nav-link">
+            About Us
+          </a>
+        </div>
+      </nav>
+    </header>
+        </>
+     );
+}
+ 
+export default NavBar;
